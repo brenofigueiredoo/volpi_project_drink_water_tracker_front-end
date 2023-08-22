bba78dbc026ac03ea69eaa44a4bbf81faee47a17
unknown <brenos93@gmail.com>
2023-08-22 18:30:13 -0300

    fix: Corrigindo o nome do componente, de historic para history

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import {Route, Routes} from "react-router-dom"
 import { UserRegister } from "../pages/UserRegister";
 import { Home } from "../pages/Home";
-import { Historic } from "../pages/Historic";
+import { History } from "../pages/History";
 
 export function RoutersMain() {
   
@@ -9,7 +9,7 @@ export function RoutersMain() {
       <Routes>
         <Route path="/cadastro" element={<UserRegister />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/historico" element={<Historic />} />
+        <Route path="/historico" element={<History />} />
         <Route path="*" element={<Home />} />
       </Routes>
     );
