1d048c053155fff2d0c1f991fa4fcc4b78ab945f
unknown <brenos93@gmail.com>
2023-11-17 20:26:25 -0300

    Add url de producao da API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://api-drink-water-tracker.onrender.com/api",
   timeout: 5000,
 });
 
@@ -14,4 +14,3 @@ export const config = () => {
     },
   };
 };
-// baseURL: "https://api-drink-water-tracker.onrender.com/api",
